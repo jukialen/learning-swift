c8feb47305e4b7a5ff922836808ed5f967eb5519
jukialen <pbidny@gmail.com>
2023-03-06 16:49:41 +0100

    Initial Commit

---
+++ learning-swift/learning_swiftApp.swift
@@ -0,0 +1,20 @@
+//
+//  learning_swiftApp.swift
+//  learning-swift
+//
+//  Created by Piotr prom on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct learning_swiftApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
